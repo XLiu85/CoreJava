8df843e2500c7048e370202873f6f2cd692fc152
Hello,duke! ^_^ <liuxukitt3@gmail.com>
2016-09-27 10:59:32 +0900

    Add files via upload

---
+++ arrays/FindCommonElementsFromArrays.java
@@ -0,0 +1,39 @@
+// arrays/FindCommonElementsFromArrays.java
+package arrays;
+
+import java.util.ArrayList;
+
+/**
+ * Problem Description: 
+ * >> How to find common elements from arrays? 
+ * Solution: 
+ * >> Following example shows how to find common elements from two arrays and store them in an array.
+ *
+ * @author kyoku-ryu
+ * @version 0.1
+ * @since 2016/09/27
+ */
+public class FindCommonElementsFromArrays {
+
+    /**
+     * FindCommonElementsFromArrays Execute Method.
+     *
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        ArrayList objArray = new ArrayList();
+        ArrayList objArray2 = new ArrayList();
+        objArray2.add(0, "common1");
+        objArray2.add(1, "common2");
+        objArray2.add(2, "notcommon");
+        objArray2.add(3, "notcommon1");
+        objArray.add(0, "common1");
+        objArray.add(1, "common2");
+        objArray.add(2, "notcommon2");
+        System.out.println("Array elements of array1" + objArray);
+        System.out.println("Array elements of array2" + objArray2);
+        objArray.retainAll(objArray2);
+        System.out.println("Array1 after retaining common elements of array2 & array1" + objArray);
+    }
+
+}
