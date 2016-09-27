5bdc21cd77dff6af5f5eeaea5857f6abf641173f
Hello,duke! ^_^ <liuxukitt3@gmail.com>
2016-09-27 17:17:04 +0900

    Add files via upload

---
+++ collections/PrintCollection.java
@@ -0,0 +1,45 @@
+// collections/PrintCollection.java
+package collections;
+
+import java.util.TreeMap;
+
+/**
+ * Problem Description:
+ * >> How to print a collection?
+ * Solution:
+ * >> Following example how to print a collection by using tMap.keySet(),tMap.values() 
+ * >> and tMap.firstKey() methods of Java Util class.
+ * 
+ * @author kyoku-ryu
+ */
+public class PrintCollection {
+
+    /**
+     * PrintCollection Execute Method.
+     *
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Tree Map Example!\n");
+        TreeMap tMap = new TreeMap();
+        tMap.put(1, "Sunday");
+        tMap.put(2, "Monday");
+        tMap.put(3, "Tuesday");
+        tMap.put(4, "Wednesday");
+        tMap.put(5, "Thursday");
+        tMap.put(6, "Friday");
+        tMap.put(7, "Saturday");
+        System.out.println("Keys of tree map: " + tMap.keySet());
+        System.out.println("Values of tree map: " + tMap.values());
+        System.out.println("Key: 5 value: " + tMap.get(5) + "\n");
+        System.out.println("First key: " + tMap.firstKey() + " Value: " + tMap.get(tMap.firstKey()) + "\n");
+        System.out.println("Last key: " + tMap.lastKey() + " Value: " + tMap.get(tMap.lastKey()) + "\n");
+        System.out.println("Removing first data: " + tMap.remove(tMap.firstKey()));
+        System.out.println("Now the tree map Keys: " + tMap.keySet());
+        System.out.println("Now the tree map contain: " + tMap.values() + "\n");
+        System.out.println("Removing last data: " + tMap.remove(tMap.lastKey()));
+        System.out.println("Now the tree map Keys: " + tMap.keySet());
+        System.out.println("Now the tree map contain: " + tMap.values());
+    }
+
+}
